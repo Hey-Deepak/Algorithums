be38fa7e748f15e5d3e1be59afb644362966b724
Deepak Choudhary <choudharydeepak990@gmail.com>
2020-01-26 22:24:13 +0530

    Update MinMax1.cpp

--- MinMax1.cpp
+++ MinMax1.cpp
@@ -1,42 +1,50 @@
-#include<iostream>
+#include <iostream>
 int min, max, arr[1000];
 using namespace std;
-void minmax(int i, int j){
+void minmax(int i, int j)
+{
 
     int max1, min1, mid;
     if (i == j)
     {
         ::max = ::min = arr[i];
         return;
-    } else if (i == j-1)
+    }
+    else if (i == j - 1)
     {
         if (arr[i] > arr[j])
         {
             ::max = arr[i];
-            ::min = arr[j];           
-        }else
+            ::min = arr[j];
+        }
+        else
         {
             ::max = arr[j];
             ::min = arr[i];
         }
         return;
     }
-    mid = (i+j)/2;
+    mid = (i + j) / 2;
     minmax(i, mid);
-    max1 = ::max; min1 = ::min;
-    minmax(mid +1, j);
-    if(max1 > ::max){
+    max1 = ::max;
+    min1 = ::min;
+    minmax(mid + 1, j);
+    if (max1 > ::max)
+    {
         ::max = max1;
     }
-    if(min1 < ::min){;
+    if (min1 < ::min)
+    {
+        ;
         ::min = min1;
     }
 }
-int main(){
+int main()
+{
     int num;
- 
+
     cout << "Enter Length of Array" << endl;
-    cin >> num ;
+    cin >> num;
     cout << "Enter Numbers:-" << endl;
     for (int i = 0; i < num; i++)
     {
@@ -44,8 +52,8 @@ int main(){
     };
     ::max = arr[0];
     ::min = arr[0];
-    minmax(0, num-1);      
+    minmax(0, num - 1);
     cout << ::min << endl;
-    cout << ::max << endl;   
+    cout << ::max << endl;
     return 0;
 }
